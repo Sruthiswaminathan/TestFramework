4a909a1e564ca62a94b580c263f3e1c82690a54e
Sruthi S <sruthi_s@epam.com>
2024-10-26 13:15:50 +0530

    Api testing completed

--- src/test/java/runnerclassess/ApiTestRunner.java
+++ src/test/java/runnerclassess/ApiTestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/resources/apitesting/crud.feature"},
+@CucumberOptions(features = {"src/test/resources/apitesting/api_test.feature"},
         glue = {"stepdefinitions"},
         plugin = {"pretty", "json:build/reports/cucumber/report.json", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"})
 public class ApiTestRunner {
